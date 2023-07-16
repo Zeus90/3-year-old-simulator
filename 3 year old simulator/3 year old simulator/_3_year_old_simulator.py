b4598c7f4ce6a29f32366fa53ff415488c4d13ec
Zeus90 <37264277+Zeus90@users.noreply.github.com>
2023-07-16 20:55:57 +0200

    Add files via upload

---
+++ 3 year old simulator/3 year old simulator/_3_year_old_simulator.py
@@ -0,0 +1,11 @@
+print("\tWelcome to the 'Three year old simulator'\n")
+print("This program simulates conversations with a Three-Year-Old child.")
+print("Try to stop the maddness.\n")
+
+response = ""
+while response != "Because.":
+    response = input("Why?\n")
+
+print("Oh. Okay!")
+
+input("\n\nPress the enter key to exit.")
